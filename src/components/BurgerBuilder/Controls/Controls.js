104ae2738e3d6e6e332af77688492b05c4cff763
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-09-08 21:30:45 +0600

    Controls Component

---
+++ src/components/BurgerBuilder/Controls/Controls.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {Card, CardBody, CardFooter, CardHeader, Button } from 'reactstrap';
+
+const controls = [
+    {label:'Salad',type:'salad'},
+    {label:'Cheese',type:'cheese'},
+    {label:'Meat',type:'meat'},
+]
+
+const BuildControl = props =>{
+    return(
+        <div className="d-flex ">
+            <div className="ms-auto" style={{ marginRight:"150px", fontWeight:"bold", fontSize:"1.2rem" }}>{props.label}</div>
+            <Button className="btn btn-danger btn-sm m-1">Less</Button>
+            <Button className="btn btn-success btn-sm m-1">More</Button>
+        </div>
+    )
+}
+const Controls = props =>{
+    return(
+        <div className="conatiner ml-md-5" style={{ textAlign:"center" }}>
+            <Card style={{ marginTop :"30px", marginBottom:"30px", textAlign:"center" }}>
+                <CardHeader style={{ backgroundColor:"#D70F64", color:"white" }}> <h4>Add Ingredients</h4> </CardHeader>
+                    <CardBody>
+                        {
+                            controls.map(item=>{
+                                return <BuildControl label={item.label} type={item.type} key= {Math.random()}/>
+                            })
+                        }
+
+
+                    </CardBody>
+
+                <CardFooter> <h5>Price: BDT</h5></CardFooter>
+            </Card>
+        </div>
+    )
+}
+
+export default Controls;
