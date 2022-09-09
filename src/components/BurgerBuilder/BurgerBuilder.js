3cf562cfb1aab748a65a831f86a4782de84654cb
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-09-09 07:54:45 +0600

    Showing Price on Card

--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -2,33 +2,44 @@ import React, {Component} from "react";
 import Burger from "./Burger/Burger";
 import Controls from "./Controls/Controls";
 
+const INGREDENT_PRICES = {
+    salad: 20,
+    cheese: 40,
+    meat: 90,
+}
+
+
+
 export default class BurgerBuilder extends Component {
     state = {
         ingredients: [
             {type: 'salad', amount: 0 },
             {type: 'cheese', amount: 0 },
             {type: 'meat', amount: 0 },
-        ]
+        ],
+        totalPrice: 80
     }
 
     addIngredientHandle = type => {
-        const ingredients = [...this.state.ingredients];
-        for(let item of ingredients){
+        const ingredient = [...this.state.ingredients];
+        const newPrice = this.state.totalPrice + INGREDENT_PRICES[type];
+        for(let item of ingredient){
             if(item.type === type) item.amount++;
         }
-        this.setState({ingredients :ingredients});
+        this.setState({ingredients :ingredient, totalPrice: newPrice});
 
     }
 
     removeIngredientHandle = type =>{
-        const ingredients = [...this.state.ingredients];
-        for(let item of ingredients){
+        const ingredient = [...this.state.ingredients];
+        const newPrice = this.state.totalPrice - INGREDENT_PRICES[type];
+        for(let item of ingredient){
             if(item.type === type) {
                 if(item.amount <=0) return;
                 item.amount--;
             }
         }
-        this.setState({ingredients :ingredients});
+        this.setState({ingredients :ingredient, totalPrice: newPrice});
 
     }
 
@@ -37,7 +48,11 @@ export default class BurgerBuilder extends Component {
         return (
             <div className="d-flex flex-md-row flex-column">
                <Burger ingredients= {this.state.ingredients}/>
-               <Controls ingredientAdded = {this.addIngredientHandle} removeIngredient = {this.removeIngredientHandle}/>
+               <Controls 
+                    ingredientAdded = {this.addIngredientHandle} 
+                    removeIngredient = {this.removeIngredientHandle}
+                    price = {this.state.totalPrice}
+               />
 
             </div>
         )
